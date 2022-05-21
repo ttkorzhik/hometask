22467467ec1582c67bf22455ac5d771076760049
ttkorzhik <tanyakorshik@gmail.com>
2022-05-21 18:19:43 +0300

    Add HT9

---
+++ slider my/src/js/script.js
@@ -0,0 +1,27 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
